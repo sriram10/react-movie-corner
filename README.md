32ce0b07baf01107ab3fb47bd307524b737dcc4e
DESKTOP-ET4K5BG\Admin <msriram1010@gmail.com>
2018-09-01 10:09:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### (In Progress)
+# React Application
+## Movie Corner
+List and Search movies. This application uses [The Movie Database(TMDb)](http://themoviedb.org) APIs for listing and search.
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+
+#### More
+Below you will find some information on how to perform common tasks.<br>
+You can find the most recent version of this guide [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
---
+++ src/components/Search/Search.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      time: null
+    }
+    this.interval = null
+    console.log(props, 'CONSTRUCTOR');
+  }
+
+  // static getDerivedStateFromProps(p) {
+  //   console.log('getDerivedStateFromProps => ', p)
+  //   return p;
+  // }
+
+  shouldComponentUpdate(p) {
+    console.log('shouldComponentUpdate', p)
+    if(p.title.length > 4) {
+      return false
+    }
+    return true;
+  }  
+
+  componentDidMount() {
+    console.log('componentDidMount');
+
+    this.interval = setInterval(() => {
+      let {count} = this.state; // destructuring
+      this.setState({
+        count: count+1,
+        time: new Date().toLocaleString()
+      })
+    }, 1000) // 1000ms
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+    if(this.state.count === 10) {
+      clearInterval(this.interval)
+    }
+  }
+
+  render() {
+    console.log('RENDER ', this.state.count)
+    return (
+      <div>
+        <h1>Search Component - {this.props.title}</h1>
+        <h2>{this.state.time}</h2>
+
+      </div>
+   )
+  }
+}
+
+export default Search
